4af8ae77940f1db067efb8ab149753be5b1f02ed
knoxjess <110634800+knoxjess@users.noreply.github.com>
2023-09-03 10:33:47 -0500

    java data cells/ wining patterns

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
     const cells = document.querySelectorAll("[data-cell]");
-    const message = document.querySelector("message");
-    const restartButton = document.querySelector("restart");
+    const message = document.getElementById("message");
+    const restartButton = document.getElementById("restart");
+
+    let currentPlayer = "X";
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+    let gameActive = true;
+
+    const winPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
     
